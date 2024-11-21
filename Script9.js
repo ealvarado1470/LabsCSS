fdcf50d1430eed6f53538e4ece86ef806bf41210
ealvarado1470 <ealvarado1470@gmail.com>
2024-11-21 17:52:59 -0500

    Answer to Q2 and Q3

--- Script9.js
+++ Script9.js
@@ -3,3 +3,11 @@ for(let i=1; i<=5;i++) {
     console.log(characters[i]);
 }
 
+const FirstName = 'Harry';
+const house = 'Gryffindor'; 
+console.log(`Welcome, ${FirstName} of ${house}!`);
+
+const characterName = 'Hermione';
+console.log (characterName.toLowerCase ())
+console.log (characterName.toUpperCase ())
+
